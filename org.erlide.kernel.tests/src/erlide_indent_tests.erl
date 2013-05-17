3691b99291776ae98f7f8bf88c0938fac1471ee4
Vlad Dumitrescu <vladdu55@gmail.com>
2013-05-17 11:24:36 +0200

    add test for indentation after $\\n

--- org.erlide.kernel.tests/src/erlide_indent_tests.erl
+++ org.erlide.kernel.tests/src/erlide_indent_tests.erl
@@ -254,6 +254,19 @@ indent_newline_char_test_() ->
             "    ok.\n",
     ?Test_indent(I, S).
 
+indent_newline_char_2_test_() ->
+    S = "" ++
+            "a()->\n"++
+            "foo(x, $\\n, y),\n"++
+            "boo(),\n" ++
+            "ok.\n",
+    I = "" ++
+            "a()->\n"++
+            "    foo(x, $\\n, y),\n"++
+            "    boo(),\n" ++
+            "    ok.\n",
+    ?Test_indent(I, S).
+
 %%
 %% Local Functions
 %%
