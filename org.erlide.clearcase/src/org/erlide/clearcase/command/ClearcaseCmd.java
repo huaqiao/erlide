9eefc59093f4238b76d2cc31b4cb5d422b5301c7
huaqiao <huaqiao.long@gmail.com>
2013-09-05 11:12:34 +0800

    Added Copyright info

--- org.erlide.clearcase/src/org/erlide/clearcase/command/ClearcaseCmd.java
+++ org.erlide.clearcase/src/org/erlide/clearcase/command/ClearcaseCmd.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Huaqiao Long.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Long Huaqiao
+ *******************************************************************************/
 package org.erlide.clearcase.command;
 
 import java.io.File;
